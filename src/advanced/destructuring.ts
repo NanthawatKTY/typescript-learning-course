6b8eba0d1763dbb78e4de6a1c0f14726401709ef
NanthawatKTY <nanthawat.tae123@gmail.com>
2022-10-11 23:44:52 +0700

    Added destructuring

---
+++ src/advanced/destructuring.ts
@@ -0,0 +1,22 @@
+//array 
+const colors:string[] = ['red', 'green', 'blue']
+const [firstColor, secondColor, thirdColor] = colors
+
+// console.log(firstColor)
+// console.log(secondColor)
+// console.log(thirdColor)
+
+//object
+const person1 = {
+    fName: 'Tae',
+    lname: 'Nama',
+    yo: 20,
+    address: "123 Main Street"
+}
+
+const {fName, lname, yo, address} = person1
+
+console.log(fName);
+console.log(lname);
+console.log(yo);
+console.log(address);
