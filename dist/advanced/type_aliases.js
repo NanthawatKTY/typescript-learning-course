0b718a6f0f05af10258c142b53979b138609aeb4
NanthawatKTY <nanthawat.tae123@gmail.com>
2022-10-11 15:46:34 +0700

    Added Type Aliases

--- dist/advanced/type_aliases.js
+++ dist/advanced/type_aliases.js
@@ -1,1 +1,21 @@
 "use strict";
+// Object
+let emp1 = {
+    id: 1,
+    name: "Tae nama",
+    salary: 100000,
+    phone: "123-456-7890",
+};
+let emp2 = {
+    id: 2,
+    name: "Tae nama 2",
+    salary: 200000,
+    phone: "123-456-2222",
+};
+// Edit value
+emp1.id = 22;
+emp1.name = "Tae nama 2";
+emp1.salary = 200000;
+emp1.phone = "098-765-4321";
+console.log(emp1);
+console.log(emp2);
---
+++ src/advanced/type_aliases.ts
@@ -0,0 +1,32 @@
+// Employee Type
+type Employee = {
+    id: number,
+    name: string,
+    salary: number,
+    phone: string,
+}
+
+// Object
+let emp1: Employee = {
+    id: 1,
+    name: "Tae nama",
+    salary: 100000,
+    phone: "123-456-7890",
+}
+
+let emp2: Employee = {
+    id: 2,
+    name: "Tae nama 2",
+    salary: 200000,
+    phone: "123-456-2222",
+}
+
+// Edit value
+emp1.id = 22
+emp1.name = "Tae nama 2"
+emp1.salary = 200000
+emp1.phone = "098-765-4321"
+
+console.log(emp1);
+console.log(emp2);
+
