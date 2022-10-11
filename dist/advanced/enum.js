f84aa848677f3642a3908bcad740414683dae41a
NanthawatKTY <nanthawat.tae123@gmail.com>
2022-10-12 01:55:35 +0700

    Added Enum

--- dist/advanced/enum.js
+++ dist/advanced/enum.js
@@ -1,1 +1,9 @@
 "use strict";
+var GameMode;
+(function (GameMode) {
+    GameMode["Easy"] = "\u0E07\u0E48\u0E32\u0E22";
+    GameMode["Medium"] = "\u0E1B\u0E32\u0E19\u0E01\u0E25\u0E32\u0E07";
+    GameMode[GameMode["Hard"] = 30] = "Hard";
+})(GameMode || (GameMode = {}));
+const select = GameMode.Medium;
+console.log(select);
---
+++ src/advanced/enum.ts
@@ -0,0 +1,8 @@
+enum GameMode{
+    Easy="ง่าย",
+    Medium="ปานกลาง",
+    Hard=30,
+}
+
+const select : GameMode = GameMode.Medium;
+console.log(select);
